41474fdc2127ef19f4e5736686b8eb45f84c4f97
SeeleNueZ <136917278+SeeleNueZ@users.noreply.github.com>
2025-03-23 22:16:44 +0800

    1

--- tools/DataLoader.py
+++ tools/DataLoader.py
@@ -2,26 +2,30 @@ import random
 
 
 class DataLoader:
-    def __init__(self, len, batch_size,shuffle=True):
+    def __init__(self, len, batch_size, shuffle=True):
         """
         初始化 DataLoader
-        :param data: 数据集（列表、NumPy 数组等）
+        :param len: 数据集（列表、NumPy 数组等）大小
         :param batch_size: 批次大小
         """
         self.shuffle = shuffle
         self.batch_size = batch_size
         self.num_samples = len
         self.data = list(range(0, self.num_samples))
-        self.num_batches = self.num_samples // self.batch_size
+        self.num_batches = max((self.num_samples // self.batch_size), 1)
 
     def __iter__(self):
         if self.shuffle:
             random.shuffle(self.data)
-        for i in range(self.num_batches):
-            start = i * self.batch_size
+        start = 0
+        while start < self.num_samples:
             end = start + self.batch_size
+            # 如果最后一组不足 batch_size，则合并到前一组
+            if end + self.batch_size > self.num_samples:
+                end = self.num_samples
             batch = self.data[start:end]
             yield batch
+            start = end
 
     def __len__(self):
         """
@@ -33,8 +37,8 @@ class DataLoader:
 # 示例使用
 if __name__ == "__main__":
     # 示例数据
-    data = list(range(10))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    batch_size = 3
+    data = list(range(100))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    batch_size = 11
 
     # 创建 DataLoader
     dataloader = DataLoader(data.__len__(), batch_size)
